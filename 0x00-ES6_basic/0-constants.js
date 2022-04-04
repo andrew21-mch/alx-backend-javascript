857d7433936dc7d8b66719ceb5aa61c3d68569cc
andrew21-mch <nfonandrew73@gmail.com>
2022-04-04 18:18:30 +0100

    started alx backend

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-function taskFirst() {
-    var task = 'I prefer const when I can.';
-    return task;
-  }
-  
-function getLast() {
-    return ' is okay';
-  }
-  
-function taskNext() {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-var ans = taskFirst() + taskNext();
-console.log(ans);
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
