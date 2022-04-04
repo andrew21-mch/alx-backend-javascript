d7d0b928d4ae018ba0408c4ced1c0431666b37db
andrew21-mch <nfonandrew73@gmail.com>
2022-04-04 19:00:48 +0100

    use keyname function

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,19 @@
+// export default function getBudgetObject(income, gdp, capita) {
+//     const budget = {
+//       income: income,
+//       gdp: gdp,
+//       capita: capita,
+//     };
+  
+//     return budget;
+//   }
+//Modify the above function’s budget object to simply use the keyname instead.
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+    return budget;
+  }
+//
