a5b6e1a8f4d53b3455726b5b473da9486cdbf772
andrew21-mch <nfonandrew73@gmail.com>
2022-04-04 18:47:37 +0100

    return number of arguments passed using rest paramenter

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,4 @@
+//write a function to return the number of arguments passed to it using the rest parameter syntax
+export default function returnHowManyArguments(...args) {
+    return args.length
+}
