9b430edab2dac7b9ab66fab7472e6ab463f470d7
andrew21-mch <nfonandrew73@gmail.com>
2022-04-04 18:40:54 +0100

    shrink a function to 1 line

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,4 @@
+//Condense the internals of the function above to 1 line - without changing the name of each function/variable.
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+    return initialNumber + (expansion1989 || 89) + (expansion2019 || 19);
+  }
