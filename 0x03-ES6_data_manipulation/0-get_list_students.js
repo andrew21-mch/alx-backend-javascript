ea7151cf9b51840e3231f9075c3c90a2b757364b
andrew21-mch <nfonandrew73@gmail.com>
2022-04-13 14:36:02 +0100

    javascript arrays

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -5,11 +5,10 @@
     Each object should have three attributes: id (Number), firstName (String), and location (String).
     */
 
-function getListStudents(){
-    const students = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-      ]
-        return students;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
