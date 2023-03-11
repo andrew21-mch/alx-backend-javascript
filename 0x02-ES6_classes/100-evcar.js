bad44d1de85a0f94334dcd2e4c3d67e13bcfd971
andrew21-mch <nfonandrew73@gmail.com>
2023-03-11 21:17:46 +0100

    ev

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,15 @@
+/* eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
+
     this._range = range;
   }
-
-  // eslint-disable-next-line
   cloneCar() {
-    return new Car[Symbol.species]();
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
   }
 }
