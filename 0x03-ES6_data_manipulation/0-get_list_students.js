bdfb19393c8e133f5f5e2a289c6464f22770dfe1
andrew21-mch <nfonandrew73@gmail.com>
2023-03-11 21:04:07 +0100

    weak

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,28 @@
 #!/opt/node12/bin/node
 
-    /*Create a function named getListStudents that returns an array of objects.
+/*Create a function named getListStudents that returns an array of objects.
 
     Each object should have three attributes: id (Number), firstName (String), and location (String).
     */
 
 export default function getListStudents() {
   return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    {
+      id: 1,
+      firstName: "Guillaume",
+      location: "San Francisco",
+    },
+
+    {
+      id: 2,
+      firstName: "James",
+      location: "Columbia",
+    },
+
+    {
+      id: 5,
+      firstName: "Serena",
+      location: "San Francisco",
+    },
   ];
 }
